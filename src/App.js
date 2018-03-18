d052e3212f7d8fb8a75fd244fbb0c0fd407f4380
taibayard <tai.bayard@gmail.com>
2018-03-17 23:41:56 -0700

    added highscore label & state

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      test:"working"
+      test:"working",
+      highscore:0
     }
   }
   componentDidMount = () =>{
@@ -18,6 +19,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <label className="high-score-lable">High Score : {this.state.highscore}</label>
         <div className="Grid">
           <Tile className="col-0 row-0 tile" />
           <Tile className="col-1 row-1 tile" />
