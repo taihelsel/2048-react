0c7ba658a1ab020419acf12cd5b201eddf0dd93e
taihelsel <tai.bayard@gmail.com>
2018-03-19 22:30:33 -0700

    can combine tiles with Right Key in console

--- src/js/tileMove.js
+++ src/js/tileMove.js
@@ -1,6 +1,31 @@
 const moveTiles = (base,selector) =>{
+    let tileValues = [];
     for(let i =0;i<base.state[selector];i++){
-        console.log(document.getElementsByClassName(selector+"-"+i));
+        let x = document.getElementsByClassName(selector+"-"+i);
+        tileValues.push(combineTiles(x));
     }
+    updateGrid(tileValues);
+}
+const combineTiles = (x) =>{
+    let activeTiles = [];
+    let lastTile = null;
+    for(let i = 0;i<x.length;i++){
+        if(x[i].textContent > 0 && x[i].textContent.length > 0){
+            if(x[i].textContent == activeTiles[lastTile]){
+                activeTiles[lastTile] = activeTiles[lastTile] * 2;
+                activeTiles.unshift(0);
+                lastTile+=2;
+            }else{
+                activeTiles.push(parseInt(x[i].textContent));
+                lastTile = i;
+            }
+        }else{
+            activeTiles.unshift(0);
+        }
+    }
+    return activeTiles;
+}
+const updateGrid = (newTiles) =>{
+    console.log(newTiles);
 }
 export default moveTiles
